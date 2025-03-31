d67c5bee61e1807cfc5960e407aca37256f45234
thiagooliveira18 <thiago.oliveira.ds18@gmail.com>
2025-03-31 18:33:41 -0300

    feat: creating application

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+
+
+import './styles/global.css';
+import './styles/theme.css';
+
+function App() {
+
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+    </>
+  )
+}
+
+export default App
